7ea8d22bcea13d5e21f9f75f0aba54c064f28c32
Onur Gungor <onurgu@gmail.com>
2013-04-02 00:44:49 +0300

    configure.sh script is modified.

--- configure.sh
+++ configure.sh
@@ -1,11 +1,25 @@
-
-
-ARCH=`uname -m`
+#!/bin/bash
 
 arch=32
-if [[ $ARCH -eq 'x86_64' ]]; then
+if [[ $arch -eq 'x86_64' ]]; then
     arch=64
 fi
 
-ln -sf MP-1.0-Linux$arch/turkish.fst turkish.fst
-ln -sf MP-1.0-Linux$arch/TurkishMorphology.so TurkishMorphology.so
+ln -sf lib/MP-1.0-Linux$arch/turkish.fst turkish.fst
+ln -sf lib/MP-1.0-Linux$arch/TurkishMorphology.so TurkishMorphology.so
+
+ln -sf lib/Rovereto-Twitter-Tokenizer/tokenizer.py tokenizer.py
+ln -sf lib/Rovereto-Twitter-Tokenizer/aux.py aux.py
+
+ln -sf lib/turkish-deasciifier/turkish turkish
+
+# require python >= 2.7
+# require easy_install
+# require virtualenv
+# require virtualenvwrapper
+
+# mkvirtualenv twitter (if doesn't exist; else workon twitter)
+#### use pip to install required packages
+#### deactivate
+
+mkdir -p captures/timelines
